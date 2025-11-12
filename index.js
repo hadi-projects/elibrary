b9d2324202d98f9fb12343a54aada88c8ad30b7b
kuromiofficials <jtgunawan007@gmail.com>
2025-11-12 23:54:43 +0700

    add role guard for book endpoint

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const { index, create, destroy, update } = require("./controller/book.controller
 const { storage, fileFilter } = require("./lib/multer")
 const multer = require('multer')
 const fav = require('./controller/favorite.controller')
+const roleGuard = require("./middleware/role_guard")
 
 
 dotenv.config()
@@ -26,9 +27,9 @@ app.post('/api/auth/register', apiKey, register);
 app.post('/api/auth/login', apiKey, login);
 
 app.get('/api/books', apiKey, jwtToken, index)
-app.post('/api/book/create', apiKey, jwtToken, upload.single('img'), create)
-app.patch('/api/book/update/:id', apiKey, jwtToken, upload.single('img'), update)
-app.delete('/api/book/delete/:id', apiKey, jwtToken, destroy)
+app.post('/api/book/create', apiKey, jwtToken, roleGuard, upload.single('img'), create)
+app.patch('/api/book/update/:id', apiKey, jwtToken, roleGuard, upload.single('img'), update)
+app.delete('/api/book/delete/:id', apiKey, jwtToken, roleGuard, destroy)
 
 app.get('/api/favorites', apiKey, jwtToken, fav.index)
 // end routes
--- middleware/role_guard.js
+++ middleware/role_guard.js
@@ -1,24 +1,9 @@
-import jwt from 'jsonwebtoken';
-import dotenv from 'dotenv';
-dotenv.config();
+export const roleGuard = (req, res, next) => {
+    const role = req.user.role;
 
-const JWT_SECRET = process.env.JWT_SECRET;
-
-export const jwtToken = (req, res, next) => {
-    const jwtAuth = req.headers['authorization'];
-    const token = jwtAuth && jwtAuth.split(' ')[1]; 
-
-    if (token == null) {
-        return res.status(401).json({ message: 'Unauthorized.' });
+    if (role == null || role != 'ADMIN') {
+        return res.status(403).json({ message: 'Unauthorized.' });
     }
 
-    jwt.verify(token, JWT_SECRET, (err, user) => {
-        if (err) {
-            return res.status(403).json({ message: 'Unauthorized.' });
-        }
-
-        req.user = user; 
-
-        next(); 
-    });
+    next(); 
 };
