5f3cb1866e57e7e533926c68c3c5f7ca621c9ec1
kuromiofficials <jtgunawan007@gmail.com>
2025-11-12 20:25:21 +0700

    update json response on book index

--- controller/book.controller.js
+++ controller/book.controller.js
@@ -7,11 +7,11 @@ dotenv.config();
 export const index = async (req, res) => {
 
     try {
-        const todos = await db.query(
+        const books = await db.query(
             'SELECT id, title, description, img FROM books WHERE deleted = 0 ORDER BY updated_at DESC', 
         );
         
-        res.status(200).json(todos);
+        res.status(200).json({'status':'success', data:books});
     } catch (error) {
         console.error('Error: ', error.toString());
         res.status(500).json({ message: 'Something went wrong.' });
