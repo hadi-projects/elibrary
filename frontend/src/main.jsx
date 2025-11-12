39512a073f68d03f1cb5c9b26031a11da1e7ee66
kuromiofficials <jtgunawan007@gmail.com>
2025-11-12 21:28:03 +0700

    add page routing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home.jsx';
+import Login from './pages/auth/Login.jsx';
+import Register from './pages/auth/Register.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+   <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
